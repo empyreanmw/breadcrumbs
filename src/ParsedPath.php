e56572c82d7b81c1efb225f91006cf08e3881581
empyreanmw <33075069+empyreanmw@users.noreply.github.com>
2018-05-10 16:30:22 +0200

    Methods get and withLinks can now be drectly called on the class

--- src/ParsedPath.php
+++ src/ParsedPath.php
@@ -3,6 +3,8 @@ namespace empyrean\breadcrumbs;
 
 class ParsedPath extends Builder
 {
+    protected $withLinks;
+
     public function build()
     {
         foreach ($this->splitPath() as $page)
@@ -15,6 +17,18 @@ class ParsedPath extends Builder
 
     public function format(FormatInterface $format)
     {
-	return $format->setUp($this->parsedPath, $this->links());
+	    return $format->setUp($this->parsedPath, $this->links());
+    }
+
+    public function withLinks()
+    {
+        $this->withLinks = $this->links();
+
+        return $this;
+    }
+
+    public function get()
+    {
+        return $this->withLinks ? [$this->parsedPath, $this->withLinks] : $this->parsedPath;
     }
 }
